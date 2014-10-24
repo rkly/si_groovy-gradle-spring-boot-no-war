c77b2372af1f0dc99c393b46f960c1a0ef776e76
lgentsch <lars.gentsch@epost-dev.de>
2014-10-24 17:40:51 +0200

    Fixed thymeleaf.

--- src/main/groovy/hello/HelloController.groovy
+++ src/main/groovy/hello/HelloController.groovy
@@ -1,14 +1,20 @@
 package hello
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+//@RestController
+@Controller
 class HelloController {
 
     @RequestMapping(value="/", method=RequestMethod.GET)
     String index() {
         return "hello"
+    }
+    
+    @RequestMapping(value="/test", method=RequestMethod.GET)
+    String test() {
+        return "test"
     }
 }
