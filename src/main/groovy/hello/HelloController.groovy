dc121cb5bafc65e9eb7b16833dc883de6659ff09
lgentsch <lars.gentsch@epost-dev.de>
2014-10-24 17:42:49 +0200

    Clean up off controller class.

--- src/main/groovy/hello/HelloController.groovy
+++ src/main/groovy/hello/HelloController.groovy
@@ -4,17 +4,11 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
-//@RestController
 @Controller
 class HelloController {
 
     @RequestMapping(value="/", method=RequestMethod.GET)
     String index() {
         return "hello"
-    }
-    
-    @RequestMapping(value="/test", method=RequestMethod.GET)
-    String test() {
-        return "test"
     }
 }
